102cb0c30c9cd7176069397299b804e5e5826bab
Surya Pratap Singh <72143906+surya123-ctrl@users.noreply.github.com>
2023-01-08 01:17:13 +0530

    done for date 7th jan

--- src/App.js
+++ src/App.js
@@ -1,8 +1,28 @@
+import "./index.css"
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  const increment = () => {
+    // count++;  wrong practice
+
+    setCount(count + 1);
+  }
+
+  const decrement = () => {
+    // count--;
+
+    setCount(count - 1);
+  }
   return (
-    <>Hello Worlds</>
-  );
+    <div>
+      <h1>Welcome to my counter app</h1>
+      <p className="btn">{count}</p>
+      <button onClick={decrement} className="btn">-</button>
+      <button onClick={increment} className="btn">+</button>
+    </div>
+  )
 }
 
 export default App;
